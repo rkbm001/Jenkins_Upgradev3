97cbb3e813dd324b71b3549dde489f24681e270d
Bhuiyan Muhaimeen <bmuhaime@versa-networks.com>
2020-06-20 01:06:58 -0400

    changed TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,7 +40,7 @@ public class TestGreeter {
   
    @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World - Thanks you!";
+    String someone = "World - Thanks you very much!";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
